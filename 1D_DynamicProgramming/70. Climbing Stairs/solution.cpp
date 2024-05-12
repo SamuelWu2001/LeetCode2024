85d08e05a3a995a370f89a15eeff4a8c868da3b4
Samuel Wu <71746159+SamuelWu2001@users.noreply.github.com>
2024-05-12 10:06:26 +0800

    Create solution.cpp

---
+++ 1D_DynamicProgramming/70. Climbing Stairs/solution.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n==1) return 1;
+        
+        vector<int> dp(n, -1);
+        dp[0] = 1;
+        dp[1] = 2;
+
+        for (int i=2;i<n;i++)
+            dp[i] = dp[i-2] + dp[i-1];
+        
+        return dp.back();
+    }
+};
