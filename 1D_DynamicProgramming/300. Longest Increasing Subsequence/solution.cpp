3ce5ca4ad89e13cd74cba1986ee6085e03e8d866
Samuel Wu <71746159+SamuelWu2001@users.noreply.github.com>
2024-06-02 13:39:26 +0800

    Update solution.cpp

--- 1D_DynamicProgramming/300. Longest Increasing Subsequence/solution.cpp
+++ 1D_DynamicProgramming/300. Longest Increasing Subsequence/solution.cpp
@@ -1,17 +1,37 @@
 class Solution {
 public:
+    // binary search method => O(nlog(n)) in Time
+    // 
     int lengthOfLIS(vector<int>& nums) {
-        vector<int> dp;
+        vector<int> sorted;
         int maxLength = 0;
         for(int i=0;i<nums.size();i++) {
-            int current = 1;
-            for (int j=0;j<i;j++) {
-                if (nums[j] < nums[i])
-                   current = max(current, dp[j]+1);   
-            }
-            dp.push_back(current);
-            maxLength = max(maxLength, current);
+            int pos = lower_bound(sorted.begin(),sorted.end(), nums[i]) - sorted.begin();
+            if (pos == sorted.size())
+                sorted.push_back(nums[i]);
+            else 
+                sorted[pos] = nums[i];
+            maxLength = max(maxLength, pos+1);        
         }
         return maxLength;
     }
+
+    // dynamic programming => O(n^2) in Time
+    //
+    // int lengthOfLIS(vector<int>& nums) {
+    //     vector<int> dp;
+    //     int maxLength = 0;
+    //     for(int i=0;i<nums.size();i++) {
+    //         int current = 1;
+    //         for (int j=0;j<i;j++) {
+    //             if (nums[j] < nums[i])
+    //                current = max(current, dp[j]+1);   
+    //         }
+    //         dp.push_back(current);
+    //         maxLength = max(maxLength, current);
+    //     }
+    //     return maxLength;
+    // }
 };
+
+
